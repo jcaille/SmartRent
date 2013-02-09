f982e2f646e55cd107193a1ec24dfea0d5185120
Jean Caillé <jcaille@gmail.com>
2013-02-09 10:47:39 +0100

    Django commit

---
+++ SmartRent_django/SmartRent_django/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'SmartRent_django.views.home', name='home'),
+    # url(r'^SmartRent_django/', include('SmartRent_django.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
